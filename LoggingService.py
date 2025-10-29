4fa5a059e665ae707bbdcbff1e34c1fdabc65c8b
Ewan Melibari <ewanhasan2004@gmail.com>
2025-10-29 23:32:59 +0300

    initial implementation

---
+++ LoggingService.py
@@ -0,0 +1,25 @@
+import logging
+
+# Singleton Architecture Logging Service
+# Still working on it :)
+
+class LoggingService:
+    _instance = None
+
+    def __new__(cls):
+        pass
+
+    def debug(self, message):
+        pass
+
+    def error(self, message):
+        pass
+
+    def info(self, message):
+        pass
+
+    def warning(self, message):
+        pass
+
+    def critical(self, message):
+        pass
